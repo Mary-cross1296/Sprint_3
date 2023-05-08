abf7b6e2c0fdd5ed19f58c902e4900575ddc74ee
Maria Petrova <maria-petrova1296@yandex.ru>
2023-05-08 03:50:56 +0300

    Переход к разделу «Булки»

--- test.py
+++ test.py
@@ -7,3 +7,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+def test_go_section_breads():
+    driver = webdriver.Chrome()
+    driver.get("https://stellarburgers.nomoreparties.site/")
+
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(
+        (By.XPATH, ".//header/nav/ul/li/a/p[text()='Конструктор']")))
+    driver.find_element(By.XPATH, ".//main/section[1]/div[1]/div[3]/span[text() = 'Начинки']").click()
+    time.sleep(3)
+    not_selected_breads = driver.find_element(By.XPATH, ".//main/section[1]/div[1]/div[1]").get_attribute('class')
+    assert not_selected_breads == "tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect", "Вкладка булки не доступна для клика"
+
+    driver.find_element(By.XPATH, ".//main/section[1]/div/div/span[text() = 'Булки']").click()
+    time.sleep(3)
+    selected_breads = driver.find_element(By.XPATH, ".//main/section[1]/div[1]/div[1]").\
+        get_attribute('class')
+    time.sleep(3)
+    assert selected_breads == "tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"
+    driver.quit()
+
--- tests/test_ section_constructor.py
+++ tests/test_ section_constructor.py
@@ -73,6 +73,24 @@ def test_click_on_stellar_burgers_logo():
 
 #Переход к разделу «Булки»
 def test_go_section_breads():
+    driver = webdriver.Chrome()
+    driver.get("https://stellarburgers.nomoreparties.site/")
+
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(
+        (By.XPATH, ".//header/nav/ul/li/a/p[text()='Конструктор']")))
+    driver.find_element(By.XPATH, ".//main/section[1]/div[1]/div[3]/span[text() = 'Начинки']").click()
+    time.sleep(3)
+    not_selected_breads = driver.find_element(By.XPATH, ".//main/section[1]/div[1]/div[1]").get_attribute('class')
+    assert not_selected_breads == "tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect", "Вкладка булки не доступна для клика"
+
+    driver.find_element(By.XPATH, ".//main/section[1]/div/div/span[text() = 'Булки']").click()
+    time.sleep(3)
+    selected_breads = driver.find_element(By.XPATH, ".//main/section[1]/div[1]/div[1]").\
+        get_attribute('class')
+    time.sleep(3)
+    assert selected_breads == "tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"
+    driver.quit()
+
 
 #Переход к разделу «Соусы»
 def test_go_section_sauces():
