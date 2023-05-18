1eb050f0d275f038f30ab9d367073be43fbdde98
Maria Petrova <maria-petrova1296@yandex.ru>
2023-05-18 22:43:56 +0300

    Убрала все комменты в файле conftest.py

--- conftest.py
+++ conftest.py
@@ -18,35 +18,3 @@ def driver():
 def fake():
     faker = Faker()
     return faker
-
-
-#@pytest.fixture()
-#def rand_name():
-    #letters_and_digits = string.ascii_letters + string.digits
-    #length = random.randint(3, 10)
-    #random_name = ''.join(random.sample(letters_and_digits, length))
-    #return random_name
-#@pytest.fixture()
-#def rand_email():
-    #letters_and_digits = string.ascii_letters + string.digits
-    #length = random.randint(3,10)
-    #random_name = ''.join(random.sample(letters_and_digits, length))
-    #random_domain = ''.join(random.sample(letters_and_digits, length))
-   #random_email = random_name + "@" + random_domain + "." + str(random.choice(['net', 'com', 'ua', 'ru', 'org']))
-    #return random_email
-
-#@pytest.fixture()
-#def rand_password():
-    #letters_and_digits = string.ascii_letters + string.digits
-    #length_password = random.randint(6, 15)
-    #random_password = ''.join(random.sample(letters_and_digits, length_password))
-    #return random_password
-
-#@pytest.fixture()
-#def invalid_rand_password():
-    #letters_and_digits = string.ascii_letters + string.digits
-    #length_password = random.randint(0, 6)
-    #random_password = ''.join(random.sample(letters_and_digits, length_password))
-    #return random_password
-
-
